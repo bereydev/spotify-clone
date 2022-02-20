ad9353743472ec73bfaed8064348d32d144c3d69
Jonathan Bereyziat <jon.jbstar@gmail.com>
2022-02-20 23:24:48 +0100

    Finalize login and profile icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
 
 const Home: NextPage = () => {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main>
-        <Sidebar></Sidebar>
+      <main className="flex">
+        <Sidebar/>
+        <Center/>
       </main>
       <div>
         {/* Player */}
