e618af9b373c3956dfa07d9f03269a9cfa1972d0
Jonathan Bereyziat <jon.jbstar@gmail.com>
2022-02-21 18:02:40 +0100

    play pause and volume works

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Center from '../components/Center'
+import Player from '../components/Player'
 import { getSession } from 'next-auth/react'
 
 const Home: NextPage = () => {
@@ -11,8 +12,8 @@ const Home: NextPage = () => {
         <Sidebar/>
         <Center/>
       </main>
-      <div>
-        {/* Player */}
+      <div className="sticky bottom-0">
+        <Player />
       </div>
     </div>
   )
